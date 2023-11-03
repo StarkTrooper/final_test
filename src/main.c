1408436195dd9f5e27b164c8094552c1d4f80f7f
Gaddiel Ramos <97007510+StarkTrooper@users.noreply.github.com>
2023-11-03 16:15:38 -0600

    New bluetooth file and SPI connection on main file

--- src/main.c
+++ src/main.c
@@ -1,43 +1,116 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <zephyr/kernel.h>
+/* #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <nrf_soc.h>
+#include <nrf_gpio.h>
+#include <nrf_delay.h>
+#include <nrf_nvic.h>
+#include <ble.h>
+#include <ble_gap.h>
+#include <sensor_driver.h> */
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#include <stdbool.h>
+#include <stdint.h>
+#include <nrf.h>
+#include "nrf_delay.h"
+#include "nrf_drv_twi.h"
+#include "nrf_ble_qwr.h"
+#include "nrf_sdh.h"
+#include "nrf_sdh_ble.h"
+#include "nrf_sdh_soc.h"
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#include "sensors.h" // Include your sensor header file
+#include "stress_detection_algorithm.h" // Include your stress detection algorithm header file
 
-/*
- * A build error on this line means your board is unsupported.
- * See the sample documentation for information on how to fix this.
- */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#define DEVICE_NAME "StressWristband" 
+#define APP_BLE_OBSERVER_PRIO 3       // BLE observer priority
 
-int main(void)
-{
-	int ret;
+static nrf_drv_twi_t m_twi = NRF_DRV_TWI_INSTANCE(0); // TWI instance
+static nrf_drv_spi_t m_spi = NRF_DRV_SPI_INSTANCE(0); // SPI instance for MAX30001
 
-	if (!gpio_is_ready_dt(&led)) {
-		return 0;
-	}
+// Initialize SPI (MAX30001 & MAX86140 sensor communication)
+static void spi_init(void) {
+    ret_code_t err_code;
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-	if (ret < 0) {
-		return 0;
-	}
+    nrf_drv_spi_config_t spi_config = NRF_DRV_SPI_DEFAULT_CONFIG;
+    spi_config.ss_pin = SPI_SS_PIN;
+    spi_config.miso_pin = SPI_MISO_PIN;
+    spi_config.mosi_pin = SPI_MOSI_PIN;
+    spi_config.sck_pin = SPI_SCK_PIN;
+    spi_config.frequency = NRF_DRV_SPI_FREQ_1M;
+    spi_config.mode = NRF_DRV_SPI_MODE_0;
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
-		k_msleep(SLEEP_TIME_MS);
-	}
-	return 0;
+    err_code = nrf_drv_spi_init(&m_spi, &spi_config, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
+}
+
+// Initialize TWI (I2C) for sensor communication
+static void twi_init(void) {
+    ret_code_t err_code;
+
+    const nrf_drv_twi_config_t twi_config = {
+        .scl                = SCL_PIN,
+        .sda                = SDA_PIN,
+        .frequency          = NRF_TWI_FREQ_100K,
+        .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+        .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_config, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+}
+
+// Initialize BLE stack
+static void ble_stack_init(void) {
+    ret_code_t err_code;
+
+    err_code = nrf_sdh_enable_request();
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_OBSERVER_PRIO);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = nrf_sdh_ble_enable(&m_erase_bonds);
+    APP_ERROR_CHECK(err_code);
+}
+
+// Main application loop
+int main(void) {
+    // Initialize microcontroller
+    // ...
+
+    // Initialize peripherals
+    // ...
+
+    // Initialize BLE stack
+    ble_stack_init();
+
+    // Initialize TWI (I2C)
+    twi_init();
+
+    // Initialize SPI for MAX30001 sensor
+    spi_init();
+
+    // Initialize sensors
+    initMAX86140(); // Initialization function for MAX86140 sensor (example)
+    initMAX30001();
+
+    while (true) {
+        // Acquire sensor data
+        readMAX86140Data(); //Reading data from MAX86140 sensor's FIFO (example)
+        readMAX30001Data();
+
+        // Run stress detection algorithm
+        runStressDetectionAlgorithm(); //Function to run the stress detection algorithm (example)
+
+        // Implement power management strategies
+        // ...
+
+        // Send data over Bluetooth
+        // ...
+
+        // Sleep or other idle tasks
+        // ...
+    }
 }
