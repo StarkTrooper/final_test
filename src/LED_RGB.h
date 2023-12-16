61937ca40acc2b2385fe010dfbc8bb9a3d1b9a9f
Eduardo Velasco R <103283094+EduardoVelRam@users.noreply.github.com>
2023-12-15 21:48:12 -0600

    LED header Dec 15th

--- src/LED_RGB.h
+++ src/LED_RGB.h
@@ -4,11 +4,18 @@
 #include <zephyr/device.h>
 #include <hal/nrf_gpio.h>
 
-// With just one RGB LED
-/*
-#define RGB_LED_NODE DT_ALIAS(gpio0) //rgb_led|gpio0   
+// With LED tire
+#define RGB_LED_NODE DT_ALIAS(gpio0)
+
+#define R_PIN DT_GPIO_PIN(RGB_LED_NODE, r_gpios)
+#define G_PIN DT_GPIO_PIN(RGB_LED_NODE, g_gpios)
+#define B_PIN DT_GPIO_PIN(RGB_LED_NODE, b_gpios)
 
 #define SLEEP_TIME_MS   1000
+
+#define RED    1, 0, 0
+#define GREEN  0, 1, 0
+#define BLUE   0, 0, 1
 
 void set_rgb_color(const struct device *dev, gpio_pin_t pin_r, gpio_pin_t pin_g, gpio_pin_t pin_b, uint8_t r, uint8_t g, uint8_t b)
 {
@@ -17,40 +24,64 @@ void set_rgb_color(const struct device *dev, gpio_pin_t pin_r, gpio_pin_t pin_g,
     gpio_pin_set(dev, pin_b, b);
 }
 
-void main(void)
-void LED3()
+void main(int val)
 {
-	
     const struct device *rgb_led = device_get_binding(DT_LABEL(RGB_LED_NODE));
-	if (!rgb_led)
+    if (!rgb_led)
     {
         printk("RGB LED device not ready\n");
         return;
     }
 
-    gpio_pin_configure(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, r_gpios), GPIO_OUTPUT);
-    gpio_pin_configure(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, g_gpios), GPIO_OUTPUT);
-    gpio_pin_configure(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, b_gpios), GPIO_OUTPUT);
+    int err = gpio_pin_configure(rgb_led, R_PIN, GPIO_OUTPUT);
+    if (err)
+    {
+        printk("Error configuring red pin: %d\n", err);
+        return;
+    }
+
+    err = gpio_pin_configure(rgb_led, G_PIN, GPIO_OUTPUT);
+    if (err)
+    {
+        printk("Error configuring green pin: %d\n", err);
+        return;
+    }
+
+    err = gpio_pin_configure(rgb_led, B_PIN, GPIO_OUTPUT);
+    if (err)
+    {
+        printk("Error configuring blue pin: %d\n", err);
+        return;
+    }
 
     while (1)
     {
-        // red
-        set_rgb_color(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, r_gpios), DT_GPIO_PIN(RGB_LED_NODE, g_gpios), DT_GPIO_PIN(RGB_LED_NODE, b_gpios), 1, 0, 0);
-        k_sleep(K_MSEC(SLEEP_TIME_MS));
-
-        // green
-        set_rgb_color(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, r_gpios), DT_GPIO_PIN(RGB_LED_NODE, g_gpios), DT_GPIO_PIN(RGB_LED_NODE, b_gpios), 0, 1, 0);
-        k_sleep(K_MSEC(SLEEP_TIME_MS));
-
+        switch (val)
+    {
+    case 1:
         // blue
-        set_rgb_color(rgb_led, DT_GPIO_PIN(RGB_LED_NODE, r_gpios), DT_GPIO_PIN(RGB_LED_NODE, g_gpios), DT_GPIO_PIN(RGB_LED_NODE, b_gpios), 0, 0, 1);
+        set_rgb_color(rgb_led, R_PIN, G_PIN, B_PIN, BLUE);
         k_sleep(K_MSEC(SLEEP_TIME_MS));
+        break;
+    case 2:
+        // red
+        set_rgb_color(rgb_led, R_PIN, G_PIN, B_PIN, RED);
+        k_sleep(K_MSEC(SLEEP_TIME_MS));
+        break;
+    case 0:
+        // green
+        set_rgb_color(rgb_led, R_PIN, G_PIN, B_PIN, GREEN);
+        k_sleep(K_MSEC(SLEEP_TIME_MS));
+        break;
+    default:
+        break;
+    }
+        
     }
 }
-*/
 
 // In case having 3 different LEDs (easiest scenario)
-
+/*
 #define LED_GREEN_NODE DT_ALIAS(led0)  // Just change pins
 #define LED_YELLOW_NODE DT_ALIAS(led1)
 #define LED_RED_NODE DT_ALIAS(led2)
@@ -100,4 +131,4 @@ void LED3()
         turn_off_led(led_red_dev, DT_GPIO_PIN(LED_RED_NODE, gpios));
     }
 }
-
+*/
